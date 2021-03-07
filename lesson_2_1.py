26ea3b77805aa0d15aeb40d43459a9f9001b64d6
RukinRW <78800301+rukinrw@users.noreply.github.com>
2021-03-07 22:51:29 +0300

    Delete lesson_2_1.py

--- lesson_2_1.py
+++
@@ -1,4 +0,0 @@
-list_a = [7, 6.5, 'text', False, complex(1, 2), set('1111dddd22222eefffFFFFfggggg'), (1, 2.9, 'some text')]
-
-for element in list_a:
-    print(f'Элемент "{element}" - ', type(element))
